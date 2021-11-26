bff549d77e2f45bc589f8703bd27c14e680b3dc9
arimulyadi <email.arimulyadi@gmail.com>
2021-11-26 23:19:37 +0700

    add file test model

--- tasks/tests/tests_models.py
+++ tasks/tests/tests_models.py
@@ -6,13 +6,13 @@ class TestModels(TestCase):
 	def setUp(self):
 		self.user = Username.objects.create(username="arimulyadi")
 		self.task = Task.objects.create(
-									title="task title",
-									description="task deskripsi",
-									date_of_creation="2021-11-26 04:24:50",
-									complete=0,
-									username_id=self.user.id,
-									priority="adanger"
-								)
+							title="task title",
+							description="task deskripsi",
+							date_of_creation="2021-11-26 04:24:50",
+							complete=0,
+							username_id=self.user.id,
+							priority="adanger"
+						)
 
 	def test_username(self):
 		self.assertEquals(self.user.username, "arimulyadi")
