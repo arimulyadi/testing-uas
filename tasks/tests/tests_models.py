411ab2f288372e3b773fb8c0c2862879bfc53806
arimulyadi <email.arimulyadi@gmail.com>
2021-11-26 23:17:22 +0700

    add file test model

--- tasks/tests/tests_models.py
+++ tasks/tests/tests_models.py
@@ -1,65 +1,22 @@
-# from django.test import TestCase
-# from budget.models import Project, Category, Expense
+from django.test import TestCase
+from tasks.models import Username, Task
 
-# class TestModels(TestCase):
+class TestModels(TestCase):
 	
-# 	def setUp(self):
-# 		self.project1 = Project.objects.create(
-# 			name='project 1',
-# 			budget=10000
-# 		)
-# 		self.category1 = Category.objects.create(
-# 			project=self.project1,
-# 			name='Development'
-# 		)
+	def setUp(self):
+		self.user = Username.objects.create(username="arimulyadi")
+		self.task = Task.objects.create(
+									title="task title",
+									description="task deskripsi",
+									date_of_creation="2021-11-26 04:24:50",
+									complete=0,
+									username_id=self.user.id,
+									priority="adanger"
+								)
 
-# 	def test_project_slug(self):
-# 		self.assertEquals(self.project1.slug, 'project-1')
+	def test_username(self):
+		self.assertEquals(self.user.username, "arimulyadi")
 
-# 	def test_project_budget_left(self):
-# 		# comment
-# 		self.assertEquals(1, 1)
-# 		# Expense.objects.create(
-# 		# 	project=self.project1,
-# 		# 	title='ex1',
-# 		# 	amount=3000,
-# 		# 	category=self.category1
-# 		# )
-# 		# 7000
-# 		# self.assertEquals(self.project1.budget_left, 7000)
-		
-
-# 	def test_project_total_transactions(self):
-# 		# comment
-# 		self.assertEquals(1, 1)
-# 		# Expense.objects.create(
-# 		# 	project=self.project1,
-# 		# 	title='ex1',
-# 		# 	amount=500,
-# 		# 	category=self.category1
-# 		# )
-# 		# Expense.objects.create(
-# 		# 	project=self.project1,
-# 		# 	title='ex1',
-# 		# 	amount=1000,
-# 		# 	category=self.category1
-# 		# )
-# 		# Expense.objects.create(
-# 		# 	project=self.project1,
-# 		# 	title='ex1',
-# 		# 	amount=500,
-# 		# 	category=self.category1
-# 		# )
-# 		# Expense.objects.create(
-# 		# 	project=self.project1,
-# 		# 	title='ex1',
-# 		# 	amount=2000,
-# 		# 	category=self.category1
-# 		# )
-# 		# Expense.objects.create(
-# 		# 	project=self.project1,
-# 		# 	title='ex1',
-# 		# 	amount=2000,
-# 		# 	category=self.category1
-# 		# )
-# 		# self.assertEquals(self.project1.total_transactions, 5)
+	def test_task(self):
+		temp_task = Task.objects.get(id=self.task.id)
+		self.assertEquals(self.task.id, temp_task.id)
